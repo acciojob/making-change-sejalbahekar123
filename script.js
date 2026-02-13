6e1aded0fb143559c01dec210ce13ebe34550f8f
sejalbahekar123 <sejalbahekar123@gmail.com>
2026-02-13 16:21:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const makeChange = (c) => {
+
+  let q = Math.floor(c / 25);
+  c = c % 25;
+ 
+  let d = Math.floor(c / 10);
+  c = c % 10;
+
+  let n = Math.floor(c / 5);
+  c = c % 5;
+
+  let p = c;  // whatever remains are pennies
+
+  return {
+    q: q,
+    d: d,
+    n: n, 
+    p: p
+  };
+}
+
   // your name here
-};
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
